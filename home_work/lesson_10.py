df5ffcefa4e521eb854b1880a7811426edece71d
vladyka130 <music319@gmail.com>
2024-04-20 06:03:39 +0300

    name.encode('utf-8')

--- home_work/lesson_10.py
+++ home_work/lesson_10.py
@@ -4,3 +4,7 @@ print(name)
 print(name.encode('utf-8'),  len(name.encode('utf-8')),  "байтів")    # кодування у utf-8 по замовчунням
 print(name.encode('utf-16'), len(name.encode('utf-16')), "байтів")
 print(name.encode('utf-32'), len(name.encode('utf-32')), "байтів")
+print("----------------------------------------------------------------------")
+
+a = b'\xd0\x92\xd1\x96\xd1\x82\xd0\xb0\xd0\xbb\xd1\x96\xd0\xb9'
+print(a.decode())                                                      # декодування (utf-8)
