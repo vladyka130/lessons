9717a093ebd2170d2b074e930860429f4344ff41
vladyka130 <music319@gmail.com>
2024-04-20 06:30:03 +0300

    encode\decode

--- home_work/lesson_10.py
+++ home_work/lesson_10.py
@@ -1,10 +1,12 @@
-name = "Віталій"
+# Даний рядок у байтовому вигляді: b'r\xc3\xa9sum\xc3\xa9' закодована в кодуванні за умовчанням utf-8.
+# Декодувати її у рядковий вигляд у кодуванні за замовчуванням.
+# Потім результат знову перетворити на байтовий, тільки вже в кодуванні 'Latin1'
+# І на кінець результат знову декодувати у рядок
+# (Результати всіх перетворень виводити на екран).
 
-print(name)
-print(name.encode('utf-8'),  len(name.encode('utf-8')),  "байтів")    # кодування у utf-8 по замовчунням
-print(name.encode('utf-16'), len(name.encode('utf-16')), "байтів")
-print(name.encode('utf-32'), len(name.encode('utf-32')), "байтів")
-print("----------------------------------------------------------------------")
-
-a = b'\xd0\x92\xd1\x96\xd1\x82\xd0\xb0\xd0\xbb\xd1\x96\xd0\xb9'
-print(a.decode())                                                      # декодування (utf-8)
+s = b'r\xc3\xa9sum\xc3\xa9'
+s_decode_utf8 = s.decode()
+print(s_decode_utf8)
+s_encode_Latin_1 = s_decode_utf8.encode('Latin1')
+print(s_encode_Latin_1)
+print(s_encode_Latin_1.decode('Latin1'))
