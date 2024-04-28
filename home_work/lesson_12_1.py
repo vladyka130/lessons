ded224ed5cadb3ac86caad55ac20d12da0e5ba37
vladyka130 <music319@gmail.com>
2024-04-28 13:37:25 +0300

    less 12 2  homeWork

--- home_work/lesson_12_1.py
+++ home_work/lesson_12_1.py
@@ -1,34 +1,46 @@
+# class Dog:
+#     color = 'Red'
+#
+#     def __init__(self, number_of_foot, tail, name):
+#         self.tail = tail
+#         self.name = name
+#         self.number_of_foot = number_of_foot
+#
+#
+#     def say(self):
+#         print('woof')
+#
+#     def go(self):
+#         for item in range(1, self.number_of_foot + 1):
+#             print(f'step on {item} foot ', end='-')
+#         print()
+#
+# #dog_1 = Dog()
+# #dog_2 = Dog()
+#
+# #dog_2.tail = False
+# #print(dog_1.tail)
+# #print(dog_2.tail)
+# #dog_3 = Dog()
+# #dog_3.age = 3
+# #print(dog_3.age)
+# print('******************************************')
+# dog_4 = Dog(4, False, 'Tuzik')
+# print(dog_4.name)
 
-# Створити батьківський клас auto, який має атрибути:
-# brand, age, cоlor, mark і weight.
-# А також методи: move, birthday і stop.
-# Методи move і stop виводять повідомлення на екран «move» та «stop», а birthday збільшує атрибут age на 1.
-# Атрибути brand, age та mark є обов'язковими під час оголошення об'єкта.
+class Cat(Dog):                                           #  успадкування класу Cat від  Dog
+    def __init__(self, number_of_foot, tail, name ):      # додавання від батьківського ше свій шоб не писати всі свої такі самі
+        super().__init__(number_of_foot, tail)
+        super().say()                                     # визов метода батьківського класу
+        self.name = name
 
-class Auto:
+    def say(self):
+        print('miay')
 
-    weight = 1700
-    color = 'red'
+class Woman(object):
+    def __init__(self, name, age, weight):
+        self.name = name
+        self._age = age                     # _ захищений атрибут
+        self.__weight = weight              #   приватний атрибут. треба  стукатись w_1._Woman__weight
 
-    def __init__(self, mark, brand, age):
-        self.brand = brand
-        self.age = age
-        self.mark = mark
-
-    def move(self):
-        print('move')
-
-    def birthday(self):
-        return self.age + 1
-
-    def stop(self):
-        print("stop")
-
-bmw = Auto('ANY', "BMW", 10)
-
-print(f'age = {bmw.age} , bmw.birthday() = age + 1 ({bmw.birthday()})')
-bmw.move()
-bmw.stop()
-print(bmw.color)
-print(bmw.weight)
-
+w_1 = Woman('Sussy', 26, 55)
