2360479be6ba04c897de56e18ba726fb91fcb751
vladyka130 <music319@gmail.com>
2024-04-28 23:34:05 +0300

    les 12 2

--- home_work/lesson_12_1.py
+++ home_work/lesson_12_1.py
@@ -1,46 +1,34 @@
-# class Dog:
-#     color = 'Red'
-#
-#     def __init__(self, number_of_foot, tail, name):
-#         self.tail = tail
-#         self.name = name
-#         self.number_of_foot = number_of_foot
-#
-#
-#     def say(self):
-#         print('woof')
-#
-#     def go(self):
-#         for item in range(1, self.number_of_foot + 1):
-#             print(f'step on {item} foot ', end='-')
-#         print()
-#
-# #dog_1 = Dog()
-# #dog_2 = Dog()
-#
-# #dog_2.tail = False
-# #print(dog_1.tail)
-# #print(dog_2.tail)
-# #dog_3 = Dog()
-# #dog_3.age = 3
-# #print(dog_3.age)
-# print('******************************************')
-# dog_4 = Dog(4, False, 'Tuzik')
-# print(dog_4.name)
+# Створити батьківський клас auto, який має атрибути:
+# brand, age, cоlor, mark і weight.
+# А також методи: move, birthday і stop.
+# Методи move і stop виводять повідомлення на екран «move» та «stop», а birthday збільшує атрибут age на 1.
+# Атрибути brand, age та mark є обов'язковими під час оголошення об'єкта.
 
-class Cat(Dog):                                           #  успадкування класу Cat від  Dog
-    def __init__(self, number_of_foot, tail, name ):      # додавання від батьківського ше свій шоб не писати всі свої такі самі
-        super().__init__(number_of_foot, tail)
-        super().say()                                     # визов метода батьківського класу
-        self.name = name
+class Auto:
+    def __init__(self, mark, brand, age, weight='', color=''):
+        self.brand = brand
+        self.age = age
+        self.mark = mark
+        self.weight = weight
+        self.color = color
 
-    def say(self):
-        print('miay')
+    def move(self):
+        print('move')
 
-class Woman(object):
-    def __init__(self, name, age, weight):
-        self.name = name
-        self._age = age                     # _ захищений атрибут
-        self.__weight = weight              #   приватний атрибут. треба  стукатись w_1._Woman__weight
+    def birthday(self):
+        return self.age + 1
 
-w_1 = Woman('Sussy', 26, 55)
+    def stop(self):
+        print("stop")
+
+bmw = Auto('ANY', "BMW", 10)
+
+
+bmw.move()
+bmw.stop()
+print(f'age = {bmw.age} , bmw.birthday = {bmw.birthday()} (age + 1)')
+
+bmw.color = 'Red'
+bmw.weight = 1700
+
+print(bmw.__dict__)
