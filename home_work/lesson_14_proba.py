0bc4d618377399ef7ecf3e37c8217d226a22e56e
vladyka130 <music319@gmail.com>
2024-05-04 06:50:57 +0300

    temp

--- home_work/lesson_14_proba.py
+++ home_work/lesson_14_proba.py
@@ -1,7 +1,12 @@
 class F():
-    def met_1(self, a, b):
-        return a + b
+
+     def met_1(self, a, b):
+         return  a + b
+
+     @staticmethod
+     def met_2(a, b):
+         return a + b
 
 a_1 = F()
-a_1.met_1(5, 5)
-print(a_1)
+print(a_1.met_1(1, 2))
+print(F.met_2(1, 2))
